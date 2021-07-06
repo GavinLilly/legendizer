af6ae5b7df5de2e496e1754c126da4352c05e4c1
Gavin Lilly <gavtherav@gmail.com>
2021-07-06 11:16:12 +0100

    feat(libtwister): add Fantastic Four keywords

--- libs/libtwister/src/lib/keywords/index.ts
+++ libs/libtwister/src/lib/keywords/index.ts
@@ -1,4 +1,5 @@
 export * as AntMan from './antMan.keywords';
 export * as DarkCity from './darkCity.keywords';
+export * as FantasticFour from './fantasticFour.keywords';
 export * as X_Men from './xMen.keywords';
 export * as Shield from './shield.keywords';
