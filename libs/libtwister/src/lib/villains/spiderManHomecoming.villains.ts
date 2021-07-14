b32faaa43daca418abc365dd1f6693df565ce388
Gavin Lilly <gavtherav@gmail.com>
2021-07-14 09:19:13 +0100

    feat(libtwister): add Spider-Man Homecoming keywords

--- libs/libtwister/src/lib/villains/spiderManHomecoming.villains.ts
+++ libs/libtwister/src/lib/villains/spiderManHomecoming.villains.ts
@@ -1,6 +1,9 @@
 import { CardType } from '../enums';
 import { GameSets } from '../gamesets';
-import { PaintTheTownRed as PaintTheTownRedKeywords } from '../keywords';
+import {
+  PaintTheTownRed as PaintTheTownRedKeywords,
+  SpiderManHomecoming as SpiderManHomecomingKeywords,
+} from '../keywords';
 
 import { IVillainGroup } from './villainGroup.interface';
 
@@ -12,12 +15,17 @@ export const SpiderManHomecoming: Record<VillainGroupNames, IVillainGroup> = {
     name: 'Salvagers',
     cardType: CardType.VILLAINGROUP,
     gameSet: GameSets.SPIDERMAN_HOMECOMING,
+    keywords: [SpiderManHomecomingKeywords.Striker],
   },
   VULTURE_TECH: {
     id: '4795a9f5-9992-4ef7-9884-c94118fd939f',
     name: 'Vulture Tech',
     cardType: CardType.VILLAINGROUP,
     gameSet: GameSets.SPIDERMAN_HOMECOMING,
-    keywords: [PaintTheTownRedKeywords.WallCrawl],
+    keywords: [
+      PaintTheTownRedKeywords.WallCrawl,
+      SpiderManHomecomingKeywords.DangerSense,
+      SpiderManHomecomingKeywords.Striker,
+    ],
   },
 };
